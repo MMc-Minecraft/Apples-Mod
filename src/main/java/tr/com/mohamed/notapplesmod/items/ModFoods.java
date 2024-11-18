ee445bf2250b11807e728775c90de2cc34c75ac6
Mohamed Mahmodi <103428364+MohammedMMC@users.noreply.github.com>
2024-11-18 23:47:15 +0300

    some effects were added

--- src/main/java/tr/com/mohamed/notapplesmod/items/ModFoods.java
+++ src/main/java/tr/com/mohamed/notapplesmod/items/ModFoods.java
@@ -14,34 +14,34 @@ import net.minecraft.registry.entry.RegistryEntry;
 
 public class ModFoods {
     //    Components
-    public static final FoodComponent WOOD_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.1F).alwaysEdible().build();
-    public static final FoodComponent CAT_APPLE = new FoodComponent.Builder().nutrition(2).saturationModifier(0.6F).alwaysEdible().build();
-    public static final FoodComponent BONES_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.1F).alwaysEdible().build();
-    public static final FoodComponent COAL_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.1F).alwaysEdible().build();
-    public static final FoodComponent COPPER_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.1F).alwaysEdible().build();
-    public static final FoodComponent DIAMOND_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.1F).alwaysEdible().build();
-    public static final FoodComponent IRON_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.1F).alwaysEdible().build();
-    public static final FoodComponent EMERALD_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.1F).alwaysEdible().build();
-    public static final FoodComponent LAPIS_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.1F).alwaysEdible().build();
-    public static final FoodComponent LEATHER_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.1F).alwaysEdible().build();
-    public static final FoodComponent NETHERITE_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.1F).alwaysEdible().build();
-    public static final FoodComponent REDSTONE_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.1F).alwaysEdible().build();
+    public static final FoodComponent WOOD_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
+    public static final FoodComponent CAT_APPLE = new FoodComponent.Builder().nutrition(6).saturationModifier(2.2F).alwaysEdible().build();
+    public static final FoodComponent BONES_APPLE = new FoodComponent.Builder().nutrition(3).saturationModifier(1.4F).alwaysEdible().build();
+    public static final FoodComponent COAL_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
+    public static final FoodComponent COPPER_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.2F).alwaysEdible().build();
+    public static final FoodComponent DIAMOND_APPLE = new FoodComponent.Builder().nutrition(4).saturationModifier(1.6F).alwaysEdible().build();
+    public static final FoodComponent IRON_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.2F).alwaysEdible().build();
+    public static final FoodComponent EMERALD_APPLE = new FoodComponent.Builder().nutrition(3).saturationModifier(1.5F).alwaysEdible().build();
+    public static final FoodComponent LAPIS_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
+    public static final FoodComponent LEATHER_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.6F).alwaysEdible().build();
+    public static final FoodComponent NETHERITE_APPLE = new FoodComponent.Builder().nutrition(4).saturationModifier(1.8F).alwaysEdible().build();
+    public static final FoodComponent REDSTONE_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
     public static final FoodComponent DIRT_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.4F).alwaysEdible().build();
     public static final FoodComponent GRAVEL_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.2F).alwaysEdible().build();
-    public static final FoodComponent LEAVES_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.1F).alwaysEdible().build();
-    public static final FoodComponent WOOL_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.1F).alwaysEdible().build();
-    public static final FoodComponent WATER_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.1F).alwaysEdible().build();
+    public static final FoodComponent LEAVES_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
+    public static final FoodComponent WOOL_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
+    public static final FoodComponent WATER_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
     public static final FoodComponent TNT_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.4F).alwaysEdible().build();
     public static final FoodComponent STONE_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.2F).alwaysEdible().build();
-    public static final FoodComponent STARS_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.1F).alwaysEdible().build();
+    public static final FoodComponent STARS_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
     public static final FoodComponent SPONGE_APPLE = new FoodComponent.Builder().nutrition(4).saturationModifier(0.6F).alwaysEdible().build();
     public static final FoodComponent SNOW_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.4F).alwaysEdible().build();
-    public static final FoodComponent SLIME_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.1F).alwaysEdible().build();
+    public static final FoodComponent SLIME_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
     public static final FoodComponent SAND_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.2F).alwaysEdible().build();
     public static final FoodComponent MILK_APPLE = new FoodComponent.Builder().nutrition(4).saturationModifier(1.8F).alwaysEdible().build();
-    public static final FoodComponent LAVA_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.1F).alwaysEdible().build();
-    public static final FoodComponent HONEY_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.1F).alwaysEdible().build();
-    public static final FoodComponent ENDER_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.1F).alwaysEdible().build();
+    public static final FoodComponent LAVA_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
+    public static final FoodComponent HONEY_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
+    public static final FoodComponent ENDER_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
     public static final FoodComponent ENCHANTED_SPONGE_APPLE = new FoodComponent.Builder().nutrition(4).saturationModifier(1.4F).alwaysEdible().build();
 
 
@@ -57,13 +57,62 @@ public class ModFoods {
             new EffectData(StatusEffects.SPEED, 20 * 120, 1, 1F),
             new EffectData(StatusEffects.JUMP_BOOST, 20 * 120, 2, 1F),
             new EffectData(StatusEffects.WIND_CHARGED, 20 * 120, 2, 1F),
-            new EffectData(StatusEffects.HERO_OF_THE_VILLAGE, 20 * 100, 2, 1F)
+            new EffectData(StatusEffects.HERO_OF_THE_VILLAGE, 20 * 100, 2, 1F),
+            new EffectData(StatusEffects.LEVITATION, 20 * 100, 1, 1F)
     );
     public static final ConsumableComponent BONES_APPLE_EFFECT = createConsumableEffect(
             new EffectData(StatusEffects.ABSORPTION, 20 * 60, 3, 1F),
             new EffectData(StatusEffects.SPEED, 20 * 60, 0, 0.8F),
             new EffectData(StatusEffects.JUMP_BOOST, 20 * 60, 0, 0.8F)
     );
+    public static final ConsumableComponent COAL_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.SPEED, 20 * 60, 3, 1F),
+            new EffectData(StatusEffects.POISON, 20 * 30, 0, 0.6F)
+    );
+    public static final ConsumableComponent COPPER_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.WEAKNESS, 20 * 60, 1, 1F),
+            new EffectData(StatusEffects.HASTE, 20 * 60, 3, 1F)
+    );
+    public static final ConsumableComponent DIAMOND_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.SPEED, 20 * 60, 1, 0.8F),
+            new EffectData(StatusEffects.HEALTH_BOOST, 20 * 60, 2, 0.8F),
+            new EffectData(StatusEffects.REGENERATION, 20 * 30, 1, 0.8F),
+            new EffectData(StatusEffects.ABSORPTION, 20 * 60, 1, 0.8F)
+    );
+    public static final ConsumableComponent IRON_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.ABSORPTION, 20 * 40, 0, 1F),
+            new EffectData(StatusEffects.INFESTED, 20 * 60, 1, 1F)
+    );
+    public static final ConsumableComponent EMERALD_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.ABSORPTION, 20 * 60, 2, 1F),
+            new EffectData(StatusEffects.HEALTH_BOOST, 20 * 60, 2, 1F),
+            new EffectData(StatusEffects.LUCK, 20 * 20, 0, 0.1F),
+            new EffectData(StatusEffects.REGENERATION, 20 * 15, 2, 1F)
+    );
+    public static final ConsumableComponent LAPIS_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.ABSORPTION, 20 * 60, 1, 1F),
+            new EffectData(StatusEffects.HASTE, 20 * 60, 2, 1F)
+    );
+    public static final ConsumableComponent LEATHER_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.SATURATION, 20 * 20, 0, 1F)
+    );
+    public static final ConsumableComponent NETHERITE_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.ABSORPTION, 20 * 60, 4, 1F),
+            new EffectData(StatusEffects.HEALTH_BOOST, 20 * 60, 3, 1F),
+            new EffectData(StatusEffects.HASTE, 20 * 60, 1, 1F),
+            new EffectData(StatusEffects.STRENGTH, 20 * 30, 1, 1F),
+            new EffectData(StatusEffects.REGENERATION, 20 * 30, 2, 1F)
+    );
+    public static final ConsumableComponent REDSTONE_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.SPEED, 20 * 60, 3, 1F),
+            new EffectData(StatusEffects.POISON, 20 * 60, 0, 0.6F)
+    );
+    public static final ConsumableComponent DIRT_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.HASTE, 20 * 60, 3, 1F),
+            new EffectData(StatusEffects.POISON, 20 * 30, 0, 0.6F),
+            new EffectData(StatusEffects.NAUSEA, 20 * 30, 2, 0.6F)
+    );
+
 
 
     public static final ConsumableComponent MILK_APPLE_EFFECT = ConsumableComponents.food()
--- src/main/java/tr/com/mohamed/notapplesmod/items/ModItems.java
+++ src/main/java/tr/com/mohamed/notapplesmod/items/ModItems.java
@@ -1,5 +1,7 @@
 package tr.com.mohamed.notapplesmod.items;
 
+import net.minecraft.component.type.ConsumableComponent;
+import net.minecraft.component.type.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -9,36 +11,43 @@ import net.minecraft.util.Identifier;
 import tr.com.mohamed.notapplesmod.NotApples;
 
 public class ModItems {
-    public static final Item WOOD_APPLE = registerItem("woodapple", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(NotApples.MOD_ID, "woodapple"))).food(ModFoods.WOOD_APPLE, ModFoods.WOOD_APPLE_EFFECT)));
-    public static final Item CAT_APPLE = registerItem("catapple", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(NotApples.MOD_ID, "catapple"))).food(ModFoods.CAT_APPLE, ModFoods.CAT_APPLE_EFFECT)));
-    public static final Item BONES_APPLE = registerItem("bonesapple", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(NotApples.MOD_ID, "bonesapple"))).food(ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT)));
-//    public static final Item COAL_APPLE = registerItem("coalapple", new Item(new Item.Settings()));
-//    public static final Item COPPER_APPLE = registerItem("copperapple", new Item(new Item.Settings()));
-//    public static final Item DIAMOND_APPLE = registerItem("diamondapple", new Item(new Item.Settings()));
-//    public static final Item IRON_APPLE = registerItem("ironapple", new Item(new Item.Settings()));
-//    public static final Item EMERALD_APPLE = registerItem("emeraldapple", new Item(new Item.Settings()));
-//    public static final Item LAPIS_APPLE = registerItem("lapisapple", new Item(new Item.Settings()));
-//    public static final Item LEATHER_APPLE = registerItem("leatherapple", new Item(new Item.Settings()));
-//    public static final Item NETHERITE_APPLE = registerItem("netheriteapple", new Item(new Item.Settings()));
-//    public static final Item REDSTONE_APPLE = registerItem("redstoneapple", new Item(new Item.Settings()));
-//    public static final Item DIRT_APPLE = registerItem("dirtapple", new Item(new Item.Settings()));
-//    public static final Item GRAVEL_APPLE = registerItem("gravelapple", new Item(new Item.Settings()));
-//    public static final Item LEAVES_APPLE = registerItem("leavesapple", new Item(new Item.Settings()));
-//    public static final Item WOOL_APPLE = registerItem("woolapple", new Item(new Item.Settings()));
-//    public static final Item WATER_APPLE = registerItem("waterapple", new Item(new Item.Settings()));
-//    public static final Item TNT_APPLE = registerItem("tntapple", new Item(new Item.Settings()));
-//    public static final Item STONE_APPLE = registerItem("stoneapple", new Item(new Item.Settings()));
-//    public static final Item STARS_APPLE = registerItem("starsapple", new Item(new Item.Settings()));
-//    public static final Item SPONGE_APPLE = registerItem("spongeapple", new Item(new Item.Settings()));
-//    public static final Item SNOW_APPLE = registerItem("snowapple", new Item(new Item.Settings()));
-//    public static final Item SLIME_APPLE = registerItem("slimeapple", new Item(new Item.Settings()));
-//    public static final Item SAND_APPLE = registerItem("sandapple", new Item(new Item.Settings()));
-//    public static final Item MILK_APPLE = registerItem("milkapple", new Item(new Item.Settings()));
-//    public static final Item LAVA_APPLE = registerItem("lavaapple", new Item(new Item.Settings()));
-//    public static final Item HONEY_APPLE = registerItem("honeyapple", new Item(new Item.Settings()));
-//    public static final Item ENDER_APPLE = registerItem("enderapple", new Item(new Item.Settings()));
-//    public static final Item ENCHANTED_SPONGE_APPLE = registerItem("enchantedspongeapple", new Item(new Item.Settings()));
+    public static final Item WOOD_APPLE = registerApple("woodapple", ModFoods.WOOD_APPLE, ModFoods.WOOD_APPLE_EFFECT);
+    public static final Item CAT_APPLE = registerApple("catapple", ModFoods.CAT_APPLE, ModFoods.CAT_APPLE_EFFECT);
+    public static final Item BONES_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item COAL_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item COPPER_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item DIAMOND_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item IRON_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item EMERALD_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item LAPIS_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item LEATHER_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item NETHERITE_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item REDSTONE_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item DIRT_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item GRAVEL_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item LEAVES_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item WOOL_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item WATER_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item TNT_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item STONE_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item STARS_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item SPONGE_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item SNOW_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item SLIME_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item SAND_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item MILK_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item LAVA_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item HONEY_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item ENDER_APPLE = registerApple("bonesapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
+//    public static final Item ENCHANTED_SPONGE_APPLE = registerApple("enchantedspongeapple", ModFoods.BONES_APPLE, ModFoods.BONES_APPLE_EFFECT);
 
+    private static Item registerApple(String name, FoodComponent foodComponent, ConsumableComponent foodEffect) {
+        return registerItem(name,
+                new Item(new Item.Settings().registryKey(
+                        RegistryKey.of(RegistryKeys.ITEM, Identifier.of(NotApples.MOD_ID, name))
+                ).food(foodComponent, foodEffect))
+        );
+    }
 
     public static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(NotApples.MOD_ID, name), item);
