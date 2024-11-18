ca51b9d840d98909d35dc4ac2a80f6fe9f648510
Mohamed Mahmodi <103428364+MohammedMMC@users.noreply.github.com>
2024-11-19 01:33:45 +0300

    New Apples has been added !

--- src/main/java/tr/com/mohamed/notapplesmod/items/ModFoods.java
+++ src/main/java/tr/com/mohamed/notapplesmod/items/ModFoods.java
@@ -14,36 +14,36 @@ import net.minecraft.registry.entry.RegistryEntry;
 
 public class ModFoods {
     //    Components
-    public static final FoodComponent WOOD_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
-    public static final FoodComponent CAT_APPLE = new FoodComponent.Builder().nutrition(6).saturationModifier(2.2F).alwaysEdible().build();
-    public static final FoodComponent BONES_APPLE = new FoodComponent.Builder().nutrition(3).saturationModifier(1.4F).alwaysEdible().build();
-    public static final FoodComponent COAL_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
-    public static final FoodComponent COPPER_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.2F).alwaysEdible().build();
-    public static final FoodComponent DIAMOND_APPLE = new FoodComponent.Builder().nutrition(4).saturationModifier(1.6F).alwaysEdible().build();
-    public static final FoodComponent IRON_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.2F).alwaysEdible().build();
-    public static final FoodComponent EMERALD_APPLE = new FoodComponent.Builder().nutrition(3).saturationModifier(1.5F).alwaysEdible().build();
-    public static final FoodComponent LAPIS_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
-    public static final FoodComponent LEATHER_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.6F).alwaysEdible().build();
-    public static final FoodComponent NETHERITE_APPLE = new FoodComponent.Builder().nutrition(4).saturationModifier(1.8F).alwaysEdible().build();
-    public static final FoodComponent REDSTONE_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
-    public static final FoodComponent DIRT_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.4F).alwaysEdible().build();
-    public static final FoodComponent GRAVEL_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.2F).alwaysEdible().build();
-    public static final FoodComponent LEAVES_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
-    public static final FoodComponent WOOL_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
-    public static final FoodComponent WATER_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
-    public static final FoodComponent TNT_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.4F).alwaysEdible().build();
-    public static final FoodComponent STONE_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.2F).alwaysEdible().build();
-    public static final FoodComponent STARS_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
-    public static final FoodComponent SPONGE_APPLE = new FoodComponent.Builder().nutrition(4).saturationModifier(0.6F).alwaysEdible().build();
-    public static final FoodComponent SNOW_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.4F).alwaysEdible().build();
-    public static final FoodComponent SLIME_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
-    public static final FoodComponent SAND_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(0.2F).alwaysEdible().build();
-    public static final FoodComponent MILK_APPLE = new FoodComponent.Builder().nutrition(4).saturationModifier(1.8F).alwaysEdible().build();
-    public static final FoodComponent LAVA_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
-    public static final FoodComponent HONEY_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
-    public static final FoodComponent ENDER_APPLE = new FoodComponent.Builder().nutrition(1).saturationModifier(1.1F).alwaysEdible().build();
-    public static final FoodComponent ENCHANTED_SPONGE_APPLE = new FoodComponent.Builder().nutrition(4).saturationModifier(1.4F).alwaysEdible().build();
 
+    public static final FoodComponent WOOD_APPLE = createAppleFoodComponent(1, 1.1F).build();
+    public static final FoodComponent CAT_APPLE = createAppleFoodComponent(6, 2.2F).build();
+    public static final FoodComponent BONES_APPLE = createAppleFoodComponent(3, 1.4F).build();
+    public static final FoodComponent COAL_APPLE = createAppleFoodComponent(1, 1.1F).build();
+    public static final FoodComponent COPPER_APPLE = createAppleFoodComponent(1, 1.2F).build();
+    public static final FoodComponent DIAMOND_APPLE = createAppleFoodComponent(4, 1.6F).build();
+    public static final FoodComponent IRON_APPLE = createAppleFoodComponent(1, 1.2F).build();
+    public static final FoodComponent EMERALD_APPLE = createAppleFoodComponent(3, 1.5F).build();
+    public static final FoodComponent LAPIS_APPLE = createAppleFoodComponent(1, 1.1F).build();
+    public static final FoodComponent LEATHER_APPLE = createAppleFoodComponent(1, 0.6F).build();
+    public static final FoodComponent NETHERITE_APPLE = createAppleFoodComponent(4, 1.8F).build();
+    public static final FoodComponent REDSTONE_APPLE = createAppleFoodComponent(1, 1.1F).build();
+    public static final FoodComponent DIRT_APPLE = createAppleFoodComponent(1, 0.6F).build();
+    public static final FoodComponent GRAVEL_APPLE = createAppleFoodComponent(1, 0.4F).build();
+    public static final FoodComponent LEAVES_APPLE = createAppleFoodComponent(1, 1.1F).build();
+    public static final FoodComponent WOOL_APPLE = createAppleFoodComponent(1, 0.4F).build();
+    public static final FoodComponent WATER_APPLE = createAppleFoodComponent(1, 1.2F).build();
+    public static final FoodComponent TNT_APPLE = createAppleFoodComponent(1, 0.4F).build();
+    public static final FoodComponent STONE_APPLE = createAppleFoodComponent(1, 0.2F).build();
+    public static final FoodComponent STARS_APPLE = createAppleFoodComponent(1, 2.8F).build();
+    public static final FoodComponent SPONGE_APPLE = createAppleFoodComponent(4, 0.6F).build();
+    public static final FoodComponent SNOW_APPLE = createAppleFoodComponent(1, 0.4F).build();
+    public static final FoodComponent SLIME_APPLE = createAppleFoodComponent(1, 1.4F).build();
+    public static final FoodComponent SAND_APPLE = createAppleFoodComponent(1, 0.4F).build();
+    public static final FoodComponent MILK_APPLE = createAppleFoodComponent(4, 1.8F).build();
+    public static final FoodComponent LAVA_APPLE = createAppleFoodComponent(1, 1.2F).build();
+    public static final FoodComponent HONEY_APPLE = createAppleFoodComponent(1, 1.1F).build();
+    public static final FoodComponent ENDER_APPLE = createAppleFoodComponent(1, 1.8F).build();
+    public static final FoodComponent ENCHANTED_SPONGE_APPLE = createAppleFoodComponent(4, 2.0F).build();
 
     // EFFECTS
     public static final ConsumableComponent WOOD_APPLE_EFFECT = createConsumableEffect(
@@ -57,8 +57,7 @@ public class ModFoods {
             new EffectData(StatusEffects.SPEED, 20 * 120, 1, 1F),
             new EffectData(StatusEffects.JUMP_BOOST, 20 * 120, 2, 1F),
             new EffectData(StatusEffects.WIND_CHARGED, 20 * 120, 2, 1F),
-            new EffectData(StatusEffects.HERO_OF_THE_VILLAGE, 20 * 100, 2, 1F),
-            new EffectData(StatusEffects.LEVITATION, 20 * 100, 1, 1F)
+            new EffectData(StatusEffects.HERO_OF_THE_VILLAGE, 20 * 100, 2, 1F)
     );
     public static final ConsumableComponent BONES_APPLE_EFFECT = createConsumableEffect(
             new EffectData(StatusEffects.ABSORPTION, 20 * 60, 3, 1F),
@@ -66,6 +65,7 @@ public class ModFoods {
             new EffectData(StatusEffects.JUMP_BOOST, 20 * 60, 0, 0.8F)
     );
     public static final ConsumableComponent COAL_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.HASTE, 20 * 60, 3, 1F),
             new EffectData(StatusEffects.SPEED, 20 * 60, 3, 1F),
             new EffectData(StatusEffects.POISON, 20 * 30, 0, 0.6F)
     );
@@ -97,30 +97,118 @@ public class ModFoods {
             new EffectData(StatusEffects.SATURATION, 20 * 20, 0, 1F)
     );
     public static final ConsumableComponent NETHERITE_APPLE_EFFECT = createConsumableEffect(
-            new EffectData(StatusEffects.ABSORPTION, 20 * 60, 4, 1F),
-            new EffectData(StatusEffects.HEALTH_BOOST, 20 * 60, 3, 1F),
+            new EffectData(StatusEffects.ABSORPTION, 20 * 30, 4, 1F),
+            new EffectData(StatusEffects.HEALTH_BOOST, 20 * 40, 3, 1F),
             new EffectData(StatusEffects.HASTE, 20 * 60, 1, 1F),
             new EffectData(StatusEffects.STRENGTH, 20 * 30, 1, 1F),
-            new EffectData(StatusEffects.REGENERATION, 20 * 30, 2, 1F)
+            new EffectData(StatusEffects.FIRE_RESISTANCE, 20 * 30, 1, 1F),
+            new EffectData(StatusEffects.REGENERATION, 20 * 60, 2, 1F)
     );
     public static final ConsumableComponent REDSTONE_APPLE_EFFECT = createConsumableEffect(
             new EffectData(StatusEffects.SPEED, 20 * 60, 3, 1F),
             new EffectData(StatusEffects.POISON, 20 * 60, 0, 0.6F)
     );
     public static final ConsumableComponent DIRT_APPLE_EFFECT = createConsumableEffect(
-            new EffectData(StatusEffects.HASTE, 20 * 60, 3, 1F),
-            new EffectData(StatusEffects.POISON, 20 * 30, 0, 0.6F),
+            new EffectData(StatusEffects.REGENERATION, 20 * 20, 0, 0.9F),
+            new EffectData(StatusEffects.POISON, 20 * 30, 0, 0.4F),
             new EffectData(StatusEffects.NAUSEA, 20 * 30, 2, 0.6F)
     );
-
-
-
+    public static final ConsumableComponent GRAVEL_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.INFESTED, 20 * 20, 2, 1F),
+            new EffectData(StatusEffects.INSTANT_HEALTH, 20 * 10, 2, 1F)
+    );
+    public static final ConsumableComponent LEAVES_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.REGENERATION, 20 * 20, 0, 0.9F),
+            new EffectData(StatusEffects.NAUSEA, 20 * 30, 2, 0.5F)
+    );
+    public static final ConsumableComponent WOOL_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.REGENERATION, 20 * 20, 0, 0.9F),
+            new EffectData(StatusEffects.NAUSEA, 20 * 30, 2, 0.2F)
+    );
+    public static final ConsumableComponent WATER_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.REGENERATION, 20 * 20, 0, 0.9F),
+            new EffectData(StatusEffects.WATER_BREATHING, 20 * 40, 1, 1F),
+            new EffectData(StatusEffects.FIRE_RESISTANCE, 20 * 40, 4, 1F)
+    );
+    public static final ConsumableComponent TNT_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.INFESTED, 20 * 60, 1, 1F),
+            new EffectData(StatusEffects.WEAVING, 20 * 60, 2, 1F),
+            new EffectData(StatusEffects.OOZING, 20 * 60, 2, 1F),
+            new EffectData(StatusEffects.WIND_CHARGED, 20 * 60, 10, 1F)
+    );
+    public static final ConsumableComponent STONE_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.REGENERATION, 20 * 20, 0, 0.9F),
+            new EffectData(StatusEffects.SLOWNESS, 20 * 60, 4, 1F),
+            new EffectData(StatusEffects.MINING_FATIGUE, 20 * 60, 3, 1F)
+    );
+    public static final ConsumableComponent STARS_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.ABSORPTION, 20 * 100, 4, 1F),
+            new EffectData(StatusEffects.FIRE_RESISTANCE, 20 * 100, 4, 1F),
+            new EffectData(StatusEffects.HEALTH_BOOST, 20 * 100, 3, 1F),
+            new EffectData(StatusEffects.HASTE, 20 * 120, 1, 1F),
+            new EffectData(StatusEffects.STRENGTH, 20 * 60, 3, 1F),
+            new EffectData(StatusEffects.INSTANT_HEALTH, 20 * 60, 2, 1F),
+            new EffectData(StatusEffects.REGENERATION, 20 * 60, 4, 1F),
+            new EffectData(StatusEffects.GLOWING, 20 * 120, 3, 1F)
+    );
+    public static final ConsumableComponent SPONGE_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.SPEED, 20 * 40, 1, 1F),
+            new EffectData(StatusEffects.HASTE, 20 * 60, 0, 1F),
+            new EffectData(StatusEffects.DOLPHINS_GRACE, 20 * 15, 0, 1F),
+            new EffectData(StatusEffects.WATER_BREATHING, 20 * 60, 1, 1F),
+            new EffectData(StatusEffects.NIGHT_VISION, 20 * 40, 1, 1F)
+    );
+    public static final ConsumableComponent SNOW_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.REGENERATION, 20 * 20, 0, 0.9F),
+            new EffectData(StatusEffects.NIGHT_VISION, 20 * 20, 3, 1F),
+            new EffectData(StatusEffects.FIRE_RESISTANCE, 20 * 30, 5, 1F)
+    );
+    public static final ConsumableComponent SLIME_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.REGENERATION, 20 * 20, 0, 0.9F),
+            new EffectData(StatusEffects.JUMP_BOOST, 20 * 60, 5, 1F),
+            new EffectData(StatusEffects.SLOWNESS, 20 * 60, 0, 1F),
+            new EffectData(StatusEffects.OOZING, 20 * 60, 3, 1F)
+    );
+    public static final ConsumableComponent SAND_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.INFESTED, 20 * 20, 2, 1F),
+            new EffectData(StatusEffects.INSTANT_HEALTH, 20 * 10, 2, 1F)
+    );
     public static final ConsumableComponent MILK_APPLE_EFFECT = ConsumableComponents.food()
             .consumeEffect(ClearAllEffectsConsumeEffect.INSTANCE)
+            .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 20 * 10, 3), 1F))
+            .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 20 * 10, 2), 1F))
+            .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 20 * 10, 5), 1F))
+            .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 20 * 10, 3), 1F))
             .build();
+    public static final ConsumableComponent LAVA_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.REGENERATION, 20 * 20, 0, 0.9F),
+            new EffectData(StatusEffects.WATER_BREATHING, 20 * 60, 1, 1F),
+            new EffectData(StatusEffects.FIRE_RESISTANCE, 20 * 60, 2, 1F),
+            new EffectData(StatusEffects.INSTANT_DAMAGE, 20 / 2, 0, 1F)
+    );
+    public static final ConsumableComponent HONEY_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.REGENERATION, 20 * 20, 0, 0.9F),
+            new EffectData(StatusEffects.SPEED, 20 * 60, 5, 1F),
+            new EffectData(StatusEffects.SLOWNESS, 20 * 60, 0, 1F),
+            new EffectData(StatusEffects.HASTE, 20 * 60, 3, 1F)
+    );
     public static final ConsumableComponent ENDER_APPLE_EFFECT = ConsumableComponents.food()
             .consumeEffect(new TeleportRandomlyConsumeEffect())
+            .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 20 * 10, 5), 1F))
+            .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 20 * 10, 5), 1F))
+            .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 20 * 10, 5), 1F))
             .build();
+    public static final ConsumableComponent ENCHANTED_SPONGE_APPLE_EFFECT = createConsumableEffect(
+            new EffectData(StatusEffects.SPEED, 20 * 80, 2, 1F),
+            new EffectData(StatusEffects.HASTE, 20 * 80, 1, 1F),
+            new EffectData(StatusEffects.DOLPHINS_GRACE, 20 * 40, 4, 1F),
+            new EffectData(StatusEffects.CONDUIT_POWER, 20 * 100, 5, 1F),
+            new EffectData(StatusEffects.RESISTANCE, 20 * 120, 2, 1F),
+            new EffectData(StatusEffects.REGENERATION, 20 * 120, 2, 1F),
+            new EffectData(StatusEffects.HEALTH_BOOST, 20 * 120, 1, 1F),
+            new EffectData(StatusEffects.WATER_BREATHING, 20 * 120, 5, 1F),
+            new EffectData(StatusEffects.NIGHT_VISION, 20 * 100, 4, 1F)
+    );
 
 
     // Helper methods
@@ -136,6 +224,13 @@ public class ModFoods {
     }
 
     private record EffectData(RegistryEntry<StatusEffect> effect, int duration, int amplifier, float chance) {
+    }
+
+    private static FoodComponent.Builder createAppleFoodComponent(int nutrition, float saturationModifier) {
+        return new FoodComponent.Builder()
+                .nutrition(nutrition)
+                .saturationModifier(saturationModifier)
+                .alwaysEdible();
     }
 
 }
